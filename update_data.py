db15f3db314d3b96351ebda1beff1bc979da9907
n1ghtmare13 <106755714+n1ghtmare13@users.noreply.github.com>
2025-01-28 23:04:29 +0100

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -16,47 +16,36 @@ def fetch_and_save_data():
         try:
             response = requests.get(url)
             response.raise_for_status()
-            response.encoding = 'utf-8' # Ustawienie UTF-8 dla pobranych danych
-            text = response.text # Pobierz tekst odpowiedzi
+            response.encoding = 'utf-8'
+            text = response.text
 
-            if filename == "substitutions.html":  # specjalne traktowanie dla strony z zastępstwami
-                soup = BeautifulSoup(text, 'html.parser')
-                # Znajdź tag meta z informacją o kodowaniu
-                meta_tag = soup.find('meta', attrs={'http-equiv': 'content-type'})
+            if filename == "substitutions.html":
+              soup = BeautifulSoup(text, 'html.parser')
+              meta_tag = soup.find('meta', attrs={'http-equiv': 'content-type'})
+              if meta_tag:
+                  meta_tag.decompose()
+              text = f'<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />{text}'
 
-                if meta_tag:
-                     # Pobierz kodowanie
-                     content = meta_tag.get('content', '').lower()
-                     if 'charset=iso-8859-2' in content:
-                        # Ustaw poprawne kodowanie (iso-8859-2) i dekoduj do utf-8
-                        text = response.text.encode('iso-8859-2', errors='ignore').decode('utf-8', errors='replace')
-
-
-            with open(filename, "w", encoding="utf-8") as file:  # Zapisz z kodowaniem UTF-8
-                file.write(text)
+            with open(filename, "w", encoding="utf-8") as file:
+               file.write(text)
             print(f"Successfully fetched and saved {filename} at {datetime.now()}")
         except requests.exceptions.RequestException as e:
-            print(f"Failed to fetch {url}: {e}")
+             print(f"Failed to fetch {url}: {e}")
         except Exception as e:
-            print(f"An unexpected error occured {e}")
+              print(f"An unexpected error occured {e}")
 
 def send_fcm_notification():
     try:
-        # Pobierz klucz konta usługi z zmiennej środowiskowej
         service_account_key_json = os.environ.get("FIREBASE_SERVICE_ACCOUNT_KEY")
         if not service_account_key_json:
             print("Firebase service account key not found in environment variables.")
             return
 
-        # Parsuj dane json do słownika
         service_account_key = json.loads(service_account_key_json)
 
-
-        # Zainicjuj Firebase Admin SDK
         cred = credentials.Certificate(service_account_key)
         firebase_admin.initialize_app(cred)
 
-        # Utwórz wiadomość FCM
         message = messaging.Message(
             notification=messaging.Notification(
                 title="Nowe dane!",
@@ -66,7 +55,6 @@ def send_fcm_notification():
             topic="all",
         )
 
-        # Wyślij wiadomość
         response = messaging.send(message)
         print(f"FCM notification sent successfully: {response}")
     except Exception as e:
